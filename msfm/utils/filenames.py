9393a2c513f22c7f3a61d04f5f1be36984ba1ffc
Tomasz Kacprzak <tomaszk@phys.ethz.ch>
2026-06-17 11:21:07 +0200

    fname

--- msfm/utils/filenames.py
+++ msfm/utils/filenames.py
@@ -33,8 +33,8 @@ def get_filename_full_maps(grid_dir="", with_bary=False, version="1.1"):
 
     return os.path.join(grid_dir, file_name)
 
+def get_filename_examples_name(index, tag, simset, with_bary=False, return_pattern=False):
 
-def get_filename_tfrecords(out_dir, index, tag, simset, with_bary=False, return_pattern=False):
     if return_pattern:
         index = "????"
     else:
@@ -45,7 +45,17 @@ def get_filename_tfrecords(out_dir, index, tag, simset, with_bary=False, return_
     else:
         file_name = f"{tag}_{simset}_dmo_{index}.tfrecord"
 
-    return os.path.join(out_dir, file_name)
+    return file_name
+
+
+def get_filename_webdataset(out_dir, index, tag, simset, with_bary=False, return_pattern=False):
+    fname = get_filename_examples_name(index, tag, simset, with_bary, return_pattern)
+    return os.path.join(out_dir, fname+'.tar')
+
+
+def get_filename_tfrecords(out_dir, index, tag, simset, with_bary=False, return_pattern=False):
+    fname = get_filename_examples_name(index, tag, simset, with_bary, return_pattern)
+    return os.path.join(out_dir, fname+'.tfrecord')
 
 
 def get_filename_z_distribution(conf, data_dir, galaxy_sample_label, i_bin):
