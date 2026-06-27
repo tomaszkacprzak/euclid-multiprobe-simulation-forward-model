9a51813d92577c9588ef729551a03edf42dde348
Tomasz Kacprzak <tomaszk@phys.ethz.ch>
2026-06-27 22:33:27 +0200

    indexing

--- msfm/apps/run_onthefly_postprocessing.py
+++ msfm/apps/run_onthefly_postprocessing.py
@@ -206,9 +206,9 @@ def main(indices, args):
     for index in indices:
 
         i_cosmo_set = index % n_cosmos
-        # index for the cosmological parameters
+        # # index for the cosmological parameters
         i_cosmo_start = (i_cosmo_set * n_cosmos_per_file) % n_cosmos
-        i_cosmo_end = ((i_cosmo_set + 1) * n_cosmos_per_file) % n_cosmos
+        i_cosmo_end = i_cosmo_start + n_cosmos_per_file
         i_perm = (index*n_examples_per_cosmo) // n_cosmos
 
         LOGGER.info(f"starting index {index} i_cosmo_set={i_cosmo_set:>5d} i_perm={i_perm:>2d}/{n_perms_per_cosmo} i_cosmo_start={i_cosmo_start:>5d}/{n_cosmos} i_cosmo_end={i_cosmo_end:>5d}/{n_cosmos}")
